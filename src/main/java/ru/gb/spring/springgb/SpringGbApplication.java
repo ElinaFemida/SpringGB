e734163fefa0f18355f50e930b888945d1ee220f
Elina <ya.ilina4@yandex.ru>
2021-06-28 22:04:00 +0300

    master branch for Spring project added

---
+++ src/main/java/ru/gb/spring/springgb/SpringGbApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.springgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gb/spring/springgb/SpringGbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.springgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
